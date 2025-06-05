4bc4b1a2155ad40660fe330a9ce3341b7c04abf1
King Bingus <bendawsdev@gmail.com>
2025-06-05 15:09:26 -0700

    stuff

--- Docs/Utils/GeneralUtils/EasyAssetLoading.md
+++ Docs/Utils/GeneralUtils/EasyAssetLoading.md
@@ -6,15 +6,21 @@ Example of loading a gameobject from a stream bundle:
 ```cs
         void OnGameInitialized(object sender, EventArgs e)
         {
-            RB = Player.Instance.bodyCollider.attachedRigidbody;
+            // NOTE: some of this code is not compatable with new versions
+            //       of gorilla tag
+            
+            // code using honeylib is up-to-date such as SetupAsset();
 
-            rHandT = Player.Instance.rightHandTransform;
-            lHandT = Player.Instance.leftHandTransform;
+            RB = GTPlayer.Instance.bodyCollider.attachedRigidbody;
 
-            var lT = GameObject.Find("Global/Local VRRig/Local Gorilla Player/rig/body/shoulder.L/upper_arm.L/forearm.L/hand.L").transform;
-            gauntletL = EasyAssetLoading.SetupAsset(Assembly.GetExecutingAssembly(), "IronMonke.Assets.gloven", "gloveL", new Vector3(-0.026f, 0.015f, -0.0015f), Quaternion.Euler(63f, 85f, 0f), lT);
-            var rT = GameObject.Find("Global/Local VRRig/Local Gorilla Player/rig/body/shoulder.R/upper_arm.R/forearm.R/hand.R").transform;
-            gauntletR = EasyAssetLoading.SetupAsset(Assembly.GetExecutingAssembly(), "IronMonke.Assets.gloven", "gloveR", new Vector3(0.02f, 0.015f, -0.0015f), Quaternion.Euler(63f, 275f, 180f), rT);
+            rHandT = GTPlayer.Instance.rightHandTransform;
+            lHandT = GTPlayer.Instance.leftHandTransform;
+
+            var lT = GameObject.Find("Player Objects/Local VRRig/Local Gorilla Player/rig/body/shoulder.L/upper_arm.L/forearm.L/hand.L").transform;
+            gauntletL = EasyAssetLoading.SetupAsset("IronMonke.Assets.gloven", "gloveL", new Vector3(-0.026f, 0.015f, -0.0015f), Quaternion.Euler(63f, 85f, 0f), lT);
+
+            var rT = GameObject.Find("Player Objects/Local VRRig/Local Gorilla Player/rig/body/shoulder.R/upper_arm.R/forearm.R/hand.R").transform;
+            gauntletR = EasyAssetLoading.SetupAsset("IronMonke.Assets.gloven", "gloveR", new Vector3(0.02f, 0.015f, -0.0015f), Quaternion.Euler(63f, 275f, 180f), rT);
 
             particlesL = gauntletL.transform.GetChild(0).GetChild(0).gameObject.GetComponent<ParticleSystem>();
             particlesR = gauntletR.transform.GetChild(0).GetChild(0).gameObject.GetComponent<ParticleSystem>();
@@ -30,6 +36,30 @@ Example of loading a gameobject from a stream bundle:
 
             initialized = true;
         }
+```
+
+## List of overloads
+```cs
+// Load an AssetBundle
+AssetBundle LoadBundle(string assetDirectory)
+AssetBundle LoadBundle(Assembly a, string assetDirectory)
+
+// Load an asset bundle and instantiate an object from it
+object LoadAssetToPrefab(string assetDirectory, string gameObjectName)
+object LoadAssetToPrefab(Assembly a, string assetDirectory, string gameObjectName)
+
+// can you guess what this does
+GameObject InstantiateAsset(string assetDirectory, string gameObjectName)
+GameObject InstantiateAsset(Assembly a, string assetDirectory, string gameObjectName)
+
+// Loads an asset bundle, grabs a game object, and sets position and rotation stuff
+GameObject SetupAsset(string assetDirectory, string gameObjectName, Vector3 pos, Quaternion eulers, Transform parent)
+GameObject SetupAsset(string assetDirectory, string gameObjectName, Vector3 pos, Quaternion eulers)
+GameObject SetupAsset(string assetDirectory, string gameObjectName, Transform parent, bool keepWorldPos)
+
+GameObject SetupAsset(Assembly a, string assetDirectory, string gameObjectName, Vector3 pos, Quaternion eulers, Transform parent)
+GameObject SetupAsset(Assembly a, string assetDirectory, string gameObjectName, Vector3 pos, Quaternion eulers)
+GameObject SetupAsset(Assembly a, string assetDirectory, string gameObjectName, Transform parent, bool keepWorldPos)
 ```
 
 This is a snippet of the new instantiation code for iron monke, it instantiates, parents, offsets and rotates the gauntlets.
--- Utils/EasyAssetLoading.cs
+++ Utils/EasyAssetLoading.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 
 namespace HoneyLib.Utils
 {
+    /*
+    * note from bingus:
+    * the assembly.getcallingassembly() was last tested with cardboard in may 2025
+    * https://github.com/sirkingbinx/cardboard/blob/master/Utils/CardboardAssetLoader.cs
+    *
+    * every code referencing these should still work because defining the assembly yourself
+    * is still avaliable as an overload
+    */
     public static class EasyAssetLoading
     {
+        public static AssetBundle LoadBundle(string assetDirectory)
+        {
+            Stream stream = Assembly.GetCallingAssembly().GetManifestResourceStream(assetDirectory);
+            return AssetBundle.LoadFromStream(stream);
+        }
+
         public static AssetBundle LoadBundle(Assembly a, string assetDirectory)
         {
             Stream stream = a.GetManifestResourceStream(assetDirectory);
             return AssetBundle.LoadFromStream(stream);
+        }
+
+        public static object LoadAssetToPrefab(string assetDirectory, string gameObjectName)
+        {
+            var ab = LoadBundle(Assembly.GetCallingAssembly(), assetDirectory);
+            var obj = ab.LoadAsset(gameObjectName);
+            ab.Unload(false);
+            return obj;
         }
 
         public static object LoadAssetToPrefab(Assembly a, string assetDirectory, string gameObjectName)
@@ -20,9 +42,34 @@ namespace HoneyLib.Utils
             return obj;
         }
 
-        public static GameObject InstantiateAsset(Assembly a, string assetDirectory, string gameObjectName)
+        public static GameObject InstantiateAsset(string assetDirectory, string gameObjectName) =>
+            GameObject.Instantiate((GameObject)LoadAssetToPrefab(Assembly.GetCallingAssembly(), assetDirectory, gameObjectName));
+
+        public static GameObject InstantiateAsset(Assembly a, string assetDirectory, string gameObjectName) =>
+            GameObject.Instantiate((GameObject)LoadAssetToPrefab(a, assetDirectory, gameObjectName));
+        
+        public static GameObject SetupAsset(string assetDirectory, string gameObjectName, Vector3 pos, Quaternion eulers, Transform parent)
         {
-            return GameObject.Instantiate((GameObject)LoadAssetToPrefab(a, assetDirectory, gameObjectName));
+            var go = InstantiateAsset(Assembly.GetCallingAssembly(), assetDirectory, gameObjectName);
+            go.transform.localPosition = pos;
+            go.transform.rotation = Quaternion.Euler(eulers.x,eulers.y,eulers.z);
+            go.transform.SetParent(parent, false);
+            return go;
+        }
+         
+        public static GameObject SetupAsset(string assetDirectory, string gameObjectName, Vector3 pos, Quaternion eulers)
+        {
+            var go = InstantiateAsset(Assembly.GetCallingAssembly(), assetDirectory, gameObjectName);
+            go.transform.position = pos;
+            go.transform.rotation = Quaternion.Euler(eulers.x, eulers.y, eulers.z);
+            return go;
+        }
+
+        public static GameObject SetupAsset(string assetDirectory, string gameObjectName, Transform parent, bool keepWorldPos)
+        {
+            var go = InstantiateAsset(Assembly.GetCallingAssembly(), assetDirectory, gameObjectName);
+            go.transform.SetParent(parent, keepWorldPos);
+            return go;
         }
 
         public static GameObject SetupAsset(Assembly a, string assetDirectory, string gameObjectName, Vector3 pos, Quaternion eulers, Transform parent)
@@ -32,7 +79,8 @@ namespace HoneyLib.Utils
             go.transform.rotation = Quaternion.Euler(eulers.x,eulers.y,eulers.z);
             go.transform.SetParent(parent, false);
             return go;
-        }   
+        }
+
         public static GameObject SetupAsset(Assembly a, string assetDirectory, string gameObjectName, Vector3 pos, Quaternion eulers)
         {
             var go = InstantiateAsset(a, assetDirectory, gameObjectName);
@@ -40,6 +88,7 @@ namespace HoneyLib.Utils
             go.transform.rotation = Quaternion.Euler(eulers.x, eulers.y, eulers.z);
             return go;
         }
+
         public static GameObject SetupAsset(Assembly a, string assetDirectory, string gameObjectName, Transform parent, bool keepWorldPos)
         {
             var go = InstantiateAsset(a, assetDirectory, gameObjectName);
