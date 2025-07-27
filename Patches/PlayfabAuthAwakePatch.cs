6484682ae163f130ca056a32bcc964b55abb8577
BzzzThe18th <69125495+BzzzThe18th@users.noreply.github.com>
2025-07-27 06:28:49 -0500

    fix dumb issue

--- Patches/PlayfabAuthAwakePatch.cs
+++ Patches/PlayfabAuthAwakePatch.cs
@@ -7,10 +7,10 @@ namespace HoneyLib.Patches
     [HarmonyPatch("Awake", MethodType.Normal)]
     static class PlayfabAuthAwakePatch
     {
-        public static void Postfix(PlatformTagJoin platform)
+        public static void Postfix(PlayFabAuthenticator __instance)
         {
             // Assign platform tag on authenticator init to avoid race conditions from the old awake delay code
-            HoneyLib.platform = platform.PlatformTag;
+            HoneyLib.platform = __instance.platform.PlatformTag;
         }
     }
 }
